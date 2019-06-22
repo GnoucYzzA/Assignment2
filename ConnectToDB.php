1cc387838a42fcadaf90640f5d36b650eebede93
Nam Cuong <cuongndngcd17192@fpt.edu.vn>
2019-06-22 09:47:25 +0700

    Update ConnectToDB.php

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -39,7 +39,7 @@ echo '<p>Students information:</p>';
 
 ?>
 <div id="container">
-<table class="table table-bordered table-condensed">
+<table class="table table-bordered table-condensed" border="1">
     <thead>
       <tr>
         <th>SneakerID</th>
