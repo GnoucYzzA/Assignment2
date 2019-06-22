729cbe65d6baec67753bb228d5bf0971156855ae
Nam Cuong <cuongndngcd17192@fpt.edu.vn>
2019-06-22 22:06:02 +0700

    adad

--- DeleteData.php
+++ DeleteData.php
@@ -40,13 +40,13 @@ background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
 					</ul>
 </div>
         </div>
-<h1 text-align: center;>DELETE DATA FROM DATABASE</h1>
+<h1 align="center">DELETE DATA FROM DATABASE</h1>
 
 <?php
 
 echo "Delete database!";
 ?>
-<ul>
+<ul align="center">
     <form name="DeleteData" action="DeleteData.php" method="POST" >
     <p text-align: center;>Toy ID:</p>
     <input type="text" name="ToyID" />
