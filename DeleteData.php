29cdc2366e712170c98c1b304f44e01b2ede20fb
Nam Cuong <cuongndngcd17192@fpt.edu.vn>
2019-06-22 21:36:28 +0700

    adad

--- DeleteData.php
+++ DeleteData.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" type="text/css" href="https://www.w3schools.com/lib/w3.css">
 <style>
 body{
   background-color: #8EC5FC;
