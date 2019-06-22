2e26d9158d30d64ba8c4f5b8b4d35d112381b9a0
Nam Cuong <cuongndngcd17192@fpt.edu.vn>
2019-06-22 09:23:18 +0700

    Update ConnectToDB.php

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -20,7 +20,7 @@ if (empty(getenv("DATABASE_URL"))){
      echo getenv("dbname");
    $db = parse_url(getenv("DATABASE_URL"));
    $pdo = new PDO("pgsql:" . sprintf(
-        "host=ec2-54-83-9-36.compute-1.amazonaws.com;port=5432;user=mnvkrpgighmovm;password=ec88450374a0be701bd72da2753e03f48af3f3e48c9644b862a58dd677d22cd5",
+        "host=ec2-54-83-9-36.compute-1.amazonaws.com;port=5432;user=mnvkrpgighmovm;password=ec88450374a0be701bd72da2753e03f48af3f3e48c9644b862a58dd677d22cd5;dbname=dfv6jafh0t2m8e",
         $db["host"],
         $db["port"],
         $db["user"],
