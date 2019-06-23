cd2700e0c0d14e6d511afc8f0914c3da376d1bbd
Nam Cuong <cuongndngcd17192@fpt.edu.vn>
2019-06-23 10:28:05 +0700

    adad

--- UpdateData.php
+++ UpdateData.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-
+<link rel="stylesheet" type="text/css" href="style.css">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
